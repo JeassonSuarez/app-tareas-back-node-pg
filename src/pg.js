d92004c181e62544350bc21dda038a8e54df86f2
jeassons <jeassonsuarez@outlook.es>
2023-02-15 10:49:31 -0500

    ultima version desplegada 1

--- src/pg.js
+++ src/pg.js
@@ -1,7 +1,7 @@
 import pgPromise from 'pg-promise';
-import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from './config.js'
+import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER, URLDB } from './config.js'
 const pgp = pgPromise({});
-const db = pgp("postgresql://postgres:7soJpdeak11MXW22HVS0@containers-us-west-171.railway.app:5993/railway");
+const db = pgp(URLDB);
 
 export default db;
 
